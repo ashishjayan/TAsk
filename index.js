669bf16c0f072873498bd9bfd56c1e04160497f0
Ananda Poudel <apoudel@stcloudstate.edu>
2018-12-03 10:10:17 -0600

    Making simple GET request

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 8081;
+
+const connection = mysql.createConnection({
+  host: "task-db.clmqh9nyjmbn.us-west-2.rds.amazonaws.com",
+  user: "root",
+  database: "taskdb",
+  password: "anandapoudel"
+});
 
 app.get("/", (req, res) =>
   res.json({
@@ -10,4 +18,11 @@ app.get("/", (req, res) =>
   })
 );
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/student-grade", (req, res) => {
+  const studentName = req.query.studentName;
+  const className = req.query.className;
+  console.log(studentName, className);
+  res.json({ studentName, className });
+});
+
+app.listen(port, () => console.log(`task listening on port ${port}!`));
